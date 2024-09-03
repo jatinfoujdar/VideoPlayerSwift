3da01939ca02874cd6d9ca3df9fa15c038231552
Jatin Foujdar <jatin321001@gmail.com>
2024-09-04 01:16:13 +0530

    Initial Commit

---
+++ VideoPlayerSwift/VideoPlayerSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerSwiftApp.swift
+//  VideoPlayerSwift
+//
+//  Created by jatin foujdar on 04/09/24.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
