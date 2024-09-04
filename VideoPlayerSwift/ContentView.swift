519119ab2ccf60affd7eb09526717feea5922850
Jatin Foujdar <jatin321001@gmail.com>
2024-09-04 15:19:59 +0530

    video palyer commit

--- VideoPlayerSwift/ContentView.swift
+++ VideoPlayerSwift/ContentView.swift
@@ -4,18 +4,19 @@
 //
 //  Created by jatin foujdar on 04/09/24.
 //
-
+import AVKit
 import SwiftUI
 
 struct ContentView: View {
+    let url = URL(string: "https://cdn.api.video/vod/vi1FBKkaYe5Y2L9wNbGDYztl/mp4/720/source.mp4?dl=1")!
+        
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VideoPlayer(player: AVPlayer(url: url))
+                    .scaledToFit()
+                    
+                
         }
-        .padding()
     }
 }
 
